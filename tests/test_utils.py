fb69563ac92dc997698f9253056828e36aefc939
Maxim <max2morozov@gmail.com>
2023-06-05 17:51:39 +0300

    open test 03 complete

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,5 +5,8 @@ def test_sort_data(test_data):
     #for z in test_data:
         #print(z['date'])
     sorted_data = sort_data(test_data)
-    for a in sorted_data:
-        print(a['date'])
+    #for a in sorted_data:
+        #print(a['date'])
+    assert [x['date'] for x in sorted_data] == \
+           ['2019-08-26T10:50:58.294041',
+            '2019-07-03T18:35:29.512364', '2018-06-30T02:08:58.425572', '2018-03-23T10:45:06.972075']

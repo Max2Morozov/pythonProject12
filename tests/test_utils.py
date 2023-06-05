11b3e48cd0a015c71f8c5e77f8fd3e16449cb032
Maxim <max2morozov@gmail.com>
2023-06-05 19:03:25 +0300

    assert format_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,5 @@
 import os
-
-from utils import sort_data, filter_data, get_data
+from utils import sort_data, filter_data, get_data, format_data
 
 
 def test_sort_data(test_data):
@@ -15,8 +14,6 @@ def test_sort_data(test_data):
     assert [x['date'] for x in sorted_data] == \
            ['2019-08-26T10:50:58.294041',
             '2019-07-03T18:35:29.512364', '2018-06-30T02:08:58.425572', '2018-03-23T10:45:06.972075']
-
-
     def test_load_data():
         assert get_data(os.path.abspath('operation_file.json')) == []
         assert get_data(os.path.abspath('test_operations.json')) == [
@@ -55,3 +52,36 @@ def test_sort_data(test_data):
     def test_sorted_by_state(tested_data):
         sort_by_state = filter_data(tested_data)
         assert [x['state'] for x in sort_by_state if x['state'] == 'EXECUTED'] == ['EXECUTED', 'EXECUTED']
+
+    def test_format_data():
+        assert format_data([{
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+        ]) == ['23.03.2018 Открытие вклада\nНовый счет  : Счет **2431\n48223.05 руб.\n']
+        assert format_data([{
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        ]) == ['23.03.2019 Перевод со счета на счет\nСчет 4481 22** **** 4719 -> Счет **1160\n43318.34 руб.\n']

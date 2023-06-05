de5b9e9c8d1de735052f142e018bbe6406d261e6
Maxim <max2morozov@gmail.com>
2023-06-06 00:54:30 +0300

    # correct utils.py #def sorted_lambda_key(x):

--- utils.py
+++ utils.py
@@ -14,10 +14,10 @@ def filter_data(data):
     data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
     return data
 
-def sorted_lambda_key(x):
-    '''Замена лямбды функции #data = sorted(data, key=lambda x: x['data'], reverse=True)
-     на key=sorted_lambda_key, reverse=True'''
-    return x['date']
+        #def sorted_lambda_key(x):
+        #'''Замена лямбды функции #data = sorted(data, key=lambda x: x['data'], reverse=True)
+        # на key=sorted_lambda_key, reverse=True'''
+        #return x['date']
 
 def sort_data(data):
     '''Сортировка транзакций через лямда функцию, списком дату '''

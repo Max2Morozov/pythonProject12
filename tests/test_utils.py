8da811cd6fb35b3510eda569334c0c8d6c6a51af
Maxim <max2morozov@gmail.com>
2023-06-05 17:42:02 +0300

    open test 02

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,8 @@ from utils import sort_data
 
 
 def test_sort_data(test_data):
-    for z in test_data:
-        print(z['date'])
-    #sort_data(test_data)
+    #for z in test_data:
+        #print(z['date'])
+    sorted_data = sort_data(test_data)
+    for a in sorted_data:
+        print(a['date'])

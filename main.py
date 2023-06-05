180b24113caea16b4cdfa5c6b61c33e93a1dc739
Maxim <max2morozov@gmail.com>
2023-06-05 15:47:04 +0300

    def filrer_data new

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from utils import get_data
+from utils import get_data, filter_data
 
 
 def main(name):
-    print('Получение данных из файла')
+    print('Получение данных из файла... ', end='')
     data = get_data()
-    print(data)
+    print("OK")
+
+    data = filter_data(data)
+
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ import json
 
 
 def get_data():
+    '''Получаем список словарей'''
     with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
-'''Получаем список словарей'''
+
 
 def filter_data(data):
-    for value in data:
-        print(value['state'])
+    '''Проверяем значение EXECUTED'''
+    data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+    return data
